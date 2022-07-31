40ebd744ddf9c2463d3727319376ac92fdb08f68
galaxy2211 <56916688+galaxy2211@users.noreply.github.com>
2022-07-31 16:08:38 +0530

    Update tinyfilemanager.php

--- tinyfilemanager.php
+++ tinyfilemanager.php
@@ -27,7 +27,7 @@ $use_auth = true;
 // Generate secure password hash - https://tinyfilemanager.github.io/docs/pwd.html
 $auth_users = array(
     'admin' => '$2y$10$/K.hjNr84lLNDt8fTXjoI.DBp6PpeyoJ.mGwrrLuCZfAwfSAGqhOW', //admin@123
-    'user' => '$2y$10$Fg6Dz8oH9fPoZ2jJan5tZuv6Z4Kp7avtQ9bDfrdRntXtPeiMAZyGO' //12345
+    'user' => '$$2y$10$hWaMvLliXUTECM8ERvoPZuEMakgJK.7oypAphmJUKPq0N6TRUqqM6' //12345
 );
 
 // Readonly users
